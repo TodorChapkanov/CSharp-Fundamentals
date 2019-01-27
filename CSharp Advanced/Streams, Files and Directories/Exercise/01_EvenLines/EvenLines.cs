556c4a2c7ef0ea44015e58950d01aaca6a2672bc
TodorChapkanov <chapkanov.t@gmail.com>
2019-01-28 00:12:30 +0200

    Homework

---
+++ CSharp Advanced/Streams, Files and Directories/Exercise/01_EvenLines/EvenLines.cs
@@ -0,0 +1,52 @@
+﻿namespace _01_EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class EvenLines
+    {
+        private const string inputPath = @"../../../text.txt";
+        private const string outputPath = @"../../../Output.txt";
+        public static void Main()
+        {
+            var couner = 0;
+            var chars = new char[] { '-', ',', '.', '!', '?' };
+
+            using (var streamReader = new StreamReader(inputPath))
+            {
+                using (var streamWriter = new StreamWriter(outputPath))
+                {
+                    while (true)
+                    {
+                        var line = streamReader.ReadLine();
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        if (couner % 2 == 0)
+                        {
+                            foreach (var symbol in chars)
+                            {
+                                if (line.Contains(symbol))
+                                {
+                                  line =  line.Replace(symbol, '@');
+                                }
+                            }
+
+                            var spliterdLine = line.Split();
+
+                           spliterdLine =  spliterdLine.Reverse().ToArray();
+                            streamWriter.WriteLine(string.Join(' ', spliterdLine));
+                        }
+                        couner++;
+                    }
+                }
+            }
+        }
+        
+    }
+
+}
---
+++ CSharp Advanced/Streams, Files and Directories/Exercise/02_LineNumbers/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _02_LineNumbers
+{
+    using System;
+    using System.IO;
+
+    public class Program
+    {
+        private const string inputPath = @"../../../Text.txt";
+        private const string outputPath = @"../../../Output.txt";
+        public static void Main()
+        {
+            var count = 1;
+            
+
+            using (var sr = new StreamReader(inputPath))
+            {
+                using (var sw = new StreamWriter(outputPath))
+                {
+                    while (true)
+                    {
+                        var line = sr.ReadLine();
+                        var charCount = 0;
+                        var letterCount = 0;
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        foreach (var symbol in line)
+                        {
+                            if (char.IsPunctuation(symbol))
+                            {
+                                charCount++;
+                            }
+                            else if (symbol != ' ' && !char.IsPunctuation(symbol))
+                            {
+                                letterCount++;
+                            }
+                           
+                        }
+
+                        line = $"{count}: {line}.({letterCount})({charCount})";
+
+                        sw.WriteLine(line);
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
